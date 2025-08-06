e9ee901ffad8a6755fd4da72eb3acb50978e33f1
Mo <mofiyinolawuyi.acl@gmail.com>
2025-08-06 18:05:25 +0000

    There is an exposed API key in /src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,17 +7,16 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "pothole-tracker-2mx78",
-  "appId": "1:1095979557118:web:01bbd4a386a1baa070c5af",
-  "storageBucket": "pothole-tracker-2mx78.firebasestorage.app",
-  "apiKey": "AIzaSyCPj-0sZwVXDowliBa8Jk7BEUv71nVr06U",
-  "authDomain": "pothole-tracker-2mx78.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1095979557118"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const storage = getStorage(app);
 
